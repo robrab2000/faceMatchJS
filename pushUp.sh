fc6a3920e692faeebc8ee06050a930d68ef6e953
Robert Homewood <roberthomewood@Roberts-MacBook-Pro.local>
2017-07-24 02:30:19 +0100

    auto commit to push to the server

--- pushUp.sh
+++ pushUp.sh
@@ -4,6 +4,6 @@ git add .
 git commit -m 'auto commit to push to the server'
 git push origin master
 
-ssh rob@vps437326.ovh.net "cd /var/www/html/faceMatchJS; sudo git pull; exit"
+ssh -t rob@vps437326.ovh.net "cd /var/www/html/faceMatchJS; sudo git pull; exit"
 
 echo "done!"
