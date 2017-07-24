d5e445444f299027fa210031af3e387f111643e3
Robert Homewood <roberthomewood@Roberts-MacBook-Pro.local>
2017-07-24 20:01:44 +0100

    test new commit function

--- update.sh
+++ update.sh
@@ -2,10 +2,14 @@
 
 if [[ $1 == "-p" ]]; then
     git add .
-    git commit -m 'auto commit to push to the server'
+    if [ -n "$2" ]; then
+        git commit -m "$2"
+    else
+        git commit -m 'auto commit to push to the server'
+    fi
     git push origin master
 fi
 
-ssh -t rob@vps437326.ovh.net "cd /var/www/html/faceMatchJS; sudo git pull; exit"
+# ssh -t rob@vps437326.ovh.net "cd /var/www/html/faceMatchJS; sudo git pull; exit"
 
 echo "done!"
